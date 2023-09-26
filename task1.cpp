0bf5dd3b8971aaf219c677ddcb713f6c461bbd04
Zsóri Ádám <bcz4bw@inf.elte.hu>
2023-09-26 11:39:43 +0200

    added DbwRecord class and dbwreader function

--- task1.cpp
+++ task1.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include "imuRecord.hpp"
+#include "dbwRecord.hpp"
 
 using namespace std;
 
@@ -57,16 +58,71 @@ void imuReader(vector<ImuRecord> *imuD){
     file.close();
 }
 
+void dbwReader(vector<DbwRecord> *dbwD){
+    ifstream file;
+    file.open("dbw.csv");
+    string line, word;
+    getline(file, line);
+    while (getline(file, line)) {
+        DbwRecord dbw;
+        dbw.Initialize();
+        stringstream ss(line);
+        int index=0;
+        while (getline(ss, word, ',')) {
+            if (word != "")
+            {
+                switch (index)
+                {
+                case 0 :
+                    dbw.timestamp = stold(word);
+                    break;
+                case 1 :
+                    dbw.speed = stold(word);
+                    break;
+                case 2 :
+                    dbw.yaw_rate = stold(word);
+                    break;
+                case 3 :
+                    dbw.v_front_left = stold(word);
+                    break;
+                case 4 :
+                    dbw.v_front_right = stold(word);
+                    break;
+                case 5 :
+                    dbw.v_rear_left = stold(word);
+                    break;
+                case 6 :
+                    dbw.v_rear_right = stold(word);
+                    break;
+                default:
+                    break;
+                }
+            }
+            index++;
+        }
+        dbwD->push_back(dbw);
+    }
+    file.close();
+}
+
 int main () {
 
     vector<ImuRecord> *imuData = new vector<ImuRecord>();
+    vector<DbwRecord> *dbwData = new vector<DbwRecord>();
     
     imuReader(imuData);
+    dbwReader(dbwData);
     
     //imu-data-check
+    /*
     for(int i=0; i<imuData->size(); i++){
         printf("%Lf,%Lf,%Lf,%Lf,%Lf,%Lf,%Lf,%Lf,%.15Lf\n", imuData->at(i).timestamp, imuData->at(i).gyro_x, imuData->at(i).gyro_y, imuData->at(i).gyro_z, imuData->at(i).acc_x, imuData->at(i).acc_y, imuData->at(i).acc_z, imuData->at(i).conf_gyro, imuData->at(i).conf_acc); 
     }
+    */
     
+    //dbw-data-check
+    for(int i=0; i<dbwData->size(); i++){
+        printf("%Lf,%Lf,%Lf,%Lf,%Lf,%Lf,%Lf\n", dbwData->at(i).timestamp, dbwData->at(i).speed, dbwData->at(i).yaw_rate, dbwData->at(i).v_front_left, dbwData->at(i).v_front_right, dbwData->at(i).v_rear_left, dbwData->at(i).v_rear_right); 
+    }
     return 0;
 }
